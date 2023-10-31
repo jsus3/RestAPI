418d4b46e8366abf34699a844d2baef28b9a82c0
Justine Mutai <justinmutai2002@gmail.com>
2023-10-30 22:32:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const express = require('express');
 
 const app = express();
 
+app.get('/api', function(req, res){
+    console.log('GET request');
+    res.end();
+
+});
+
 //listen for requests
 app.listen(process.env.port || 4000, function(){
     console.log('now listening for requents');
