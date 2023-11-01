f328482733738dbcfed7c86dfda6e9aa6240c838
Justine Mutai <justinmutai2002@gmail.com>
2023-10-31 22:25:38 -0700

    Update ninja.js

--- models/ninja.js
+++ models/ninja.js
@@ -3,4 +3,17 @@ const Schema = mongoose.Schema;
 
 //create ninja Schema & model
 
-const N
+const NinjaSchema = new Schema({
+    name: {
+        type: String, required: [true, 'Name field is required']
+    },
+    rank: {
+        type: String
+    },
+    availability:{
+        type: Boolean, default: false
+    }
+    //add in geo location
+
+
+});
